bfd9beccda667e07c7317a9f9d569981709d2c53
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-07 22:29:45 +0200

    getData

--- website/app.js
+++ website/app.js
@@ -1,5 +1,26 @@
 /* Global Variables */
-
+const baseURL = 'http://api.openweathermap.org/geo/1.0/zip?zip=';
+const apiKey = 'e3782a4dd6b0412bda125cbd7b365830';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const getData = async(baseURL , key)=>{
+    const zipCode = document.querySelector('#zip').value;
+    console.log(zipCode);
+    const res = await fetch(baseURL+zipCode+'&appid='+apiKey);
+    try {
+        const allData = await res.json();
+        console.log(allData);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+document.querySelector('#generate').addEventListener('click' , function() {
+    getData(baseURL , apiKey);
+});
+
+
+
