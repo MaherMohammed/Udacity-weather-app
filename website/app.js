f9e003b42c998892cbe358a770081490d3ba2512
MaherMohammed <maher.mohammed10.97@gmail.com>
2021-05-10 15:25:42 +0200

    fix errors

--- website/app.js
+++ website/app.js
@@ -1,9 +1,9 @@
 /* Global Variables */
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-const apiKey = 'e3782a4dd6b0412bda125cbd7b365830';
+const apiKey = 'e3782a4dd6b0412bda125cbd7b365830&units=imperial';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 const getData = async(baseURL , key)=>{
     const zipCode = document.querySelector('#zip').value;
